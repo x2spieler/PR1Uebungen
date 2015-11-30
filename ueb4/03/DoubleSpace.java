249fcc36a2d4f61f42405ccfc63ac21fe93c58f2
back2rl <leonardoertelt@web.de>
2015-11-30 13:45:11 +0100

    added ueb4 03

---
+++ ueb4/03/DoubleSpace.java
@@ -0,0 +1,46 @@
+/**
+ *	Mo 30. Nov 13:43:35 CET 2015
+ *
+ *	PR1, WS2015/16
+ *
+ *	Leonard Oertelt
+ *	Matrikelnummer 1276156
+ *	leonard.oertelt@stud.hs-hannover.de
+ * 
+ *	Julian Opitz
+ * 	Matrikelnummer 1302082
+ * 	julian.opitz@stud.hs-hannover.de
+ *
+ *	-----------------------------------------
+ *	Programmbeschreibung
+ */
+import java.io.*;
+import java.util.*;
+
+public class DoubleSpace {
+	public static void main(String[] args) throws IOException {
+		doubleSpace("1.txt","2.txt");
+
+
+	}
+	public static void doubleSpace(String file1, String file2) throws IOException {
+
+		File fRead = new File(file1);
+
+		Scanner	scanFRead = new Scanner(fRead);
+
+		PrintStream fWrite = new PrintStream(new File(file2));
+
+		if(scanFRead.hasNextLine()) {
+			fWrite.println(scanFRead.nextLine());
+			while (scanFRead.hasNextLine()) {
+				fWrite.println();
+				fWrite.println();
+				fWrite.println(scanFRead.nextLine());
+			}
+		}
+		fWrite.close();
+		scanFRead.close();
+
+	}
+}
