44b8f249ea6ed8ec1812d61cd280cf921ac0269a
Back2RL <leonardoertelt@gmail.de>
2015-12-06 16:44:17 +0100

    Fertige Aufgabe 01 Ueb5

--- ueb5/01/WordLengths.java
+++ ueb5/01/WordLengths.java
@@ -1,16 +1,16 @@
 /**
- *	Fr 4. Dez 16:56:03 CET 2015
- * <p>
+ * So 06. Dez 16:25:00 CEST 2015
+ *
  * PR1, WS2015/16
- * <p>
+ *
  * Leonard Oertelt
  * Matrikelnummer 1276156
  * leonard.oertelt@stud.hs-hannover.de
- * <p>
+ *
  * Julian Opitz
  * Matrikelnummer 1302082
  * julian.opitz@stud.hs-hannover.de
- * <p>
+ *
  * -----------------------------------------
  * Programmbeschreibung
  */
@@ -20,48 +20,42 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class WordLengths {
-
     public static void main(String[] args) {
-        String fileName = null;
-        if (args.length > 0) {
-            fileName = args[0];
-        } else {
-            throw new IllegalArgumentException("Es wurde keine Datei angegeben");
-
+        try {
+            wordLengths(args[0]);
         }
-	System.out.println("\""+fileName+"\"");
-        wordLengths(fileName);
-
+        catch (IndexOutOfBoundsException e){
+            System.out.println("Es wurde kein Dateiname übergeben.");
+        }
     }
 
-    public static void wordLengths(String fileName) {
-        File f = new File(fileName);
-	Scanner read = null;
+    public static void wordLengths(String filename) {
+        File file = new File(filename);
+        Scanner readFile = null;
         try {
-            read = new Scanner(f);
-
-            int count = 0;
-            while (read.hasNextLine()) {
-                count++;
-                int numTokens = countTokensInString(read.nextLine());
-                System.out.print(count + ": " + numTokens + "\t");
-                printStars(numTokens);
+            readFile = new Scanner(file);
+            int[] lengths = new int[80];
+            while (readFile.hasNext()) {
+                String currentWord = readFile.next();
+                lengths[currentWord.length() - 1]++;
             }
 
+            int lastIndex = 0;
+            for (int i = 0; i < 80; i++) {
+                if (lengths[i] > 0) {
+                    lastIndex = i;
+                }
+            }
 
-        } catch (FileNotFoundException error) {
-            System.out.println("Datei existiert nicht");
-        }
-    }
+            for (int i = 0; i <= lastIndex; i++) {
+                System.out.print((i + 1) + ": " + lengths[i] + "\t");
+                printStars(lengths[i]);
+            }
 
-    public static int countTokensInString(String line) {
-        Scanner readLine = new Scanner(line);
-        int cnt = 0;
-        while (readLine.hasNext()) {
-		readLine.next();
-            cnt++;
+        } catch (FileNotFoundException e) {
+            System.out.println("Datei \"" + filename + "\" kann nicht gefunden werden.");
         }
-        return cnt;
+
     }
 
     public static void printStars(int n) {
